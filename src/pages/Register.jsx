b20f5b75cb2a61480477cc45184fb2410599635c
Jonathan Jiang <jonathan.jiang01@sjsu.edu>
2025-06-10 22:04:25 +0800

    fix some bug

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -25,33 +25,49 @@ const Register = () => {
       <PageTitle title="Create an account"></PageTitle>
       <div className="grid relative w-screen h-dvh grid-cols-1 lg:grid-cols-[1fr_1.2fr] lg:gap-2 p-2">
         <div className="flex flex-col">
-          <Link>
+          <Link to="/" className="mx-auto max-w-max lg:mx-0">
             <img
-              src={logoDark}
-              alt="Company Logo Dark"
-              className=""
+              src={logoLight}
+              alt="Company Logo Light"
+              className="dark:hidden"
               width={133}
               height={24}
             ></img>
             <img
-              src={logoLight}
-              alt="Company Logo Light"
-              className=""
+              src={logoDark}
+              alt="Company Logo Dark"
+              className="hidden dark:block"
               width={133}
               height={24}
             ></img>
           </Link>
-          <div className="">
-            <h2> Create an account</h2>
-            <p>
+          {/* 與 max-w-[480px] 結合時的效果：
+          html
+          Copy
+          Edit
+          <div className="w-full max-w-[480px] mx-auto">
+          這樣的組合代表：
+          w-full：預設佔滿父元素的寬度
+          max-w-[480px]：限制最大寬度不超過 480px mx-auto：在水平方向置中 */}
+          <div className="flex flex-col gap-2 max-w-[480px] mx-auto w-full">
+            <h2
+              className="text-displaySmall font-semibold text-(--text-dark-primary) dark:text-text-(--text-light-primary)
+                          text-center"
+            >
+              Create an account
+            </h2>
+            <p
+              className="text-bodyLarge text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant mt-1
+            mb-5 text-center px-2"
+            >
               Register now to receive free and accurate auto repair estimates.
             </p>
-            <Form method="POST" className="">
+            <Form method="POST" className="grid grid-cols-1 gap-4">
               <TextField
                 type="text"
                 name="name"
-                label="full name"
-                placeHolder="Full Name"
+                label="Full name"
+                placeHolder="Enter your full Name"
                 required={true}
                 autoFocus={true} // 這個輸入框會在頁面打開時自動聚焦，使用者可以立刻開始輸入，不用點一下
               />
@@ -77,8 +93,8 @@ const Register = () => {
                 Sign in
               </Link>
             </p>
+            <p className="">&copy; 2025 johnjiangtw0804. All right reserved</p>
           </div>
-          <p className="">&copy; 2025 johnjiangtw0804. All right reserved</p>
         </div>
         <div className="img-cover">
           <img src={banner} alt="" className=""></img>
